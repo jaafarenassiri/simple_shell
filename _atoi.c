7134f3fe75106d5dbda64a1263295dff62e8aaca
Achrafans <achrafans3434@gmail.com>
2023-08-23 17:02:10 +0100

    nv

--- _atoi.c
+++ _atoi.c
@@ -1,23 +1,23 @@
 #include "shell.h"
 
 /**
- * interactive ---> return true if shell is interactive mode
-	* @inf: --> struct address
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
  *
- * Return: 1 interactive mode-- 0 otherwise
+ * Return: 1 if interactive mode, 0 otherwise
  */
-int interactive(info_t *inf)
+int interactive(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && inf->readfd <= 2);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_dlm --> checks if character is a delimeter
- * @c:-> the char to check
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
  * @delim: the delimeter string
- * Return: 1 true, 0 false
+ * Return: 1 if true, 0 if false
  */
-int is_dlm(char c, char *delim)
+int is_delim(char c, char *delim)
 {
 	while (*delim)
 		if (*delim++ == c)
@@ -26,12 +26,12 @@ int is_dlm(char c, char *delim)
 }
 
 /**
- * _isalph --> checks for alphabetic character
+ * _isalpha - checks for alphabetic character
  * @c: The character to input
- * Return: 1  is alphabetic, 0 otherwise
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
 
-int _isalph(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
@@ -40,26 +40,26 @@ int _isalph(int c)
 }
 
 /**
- * _atoi --> converts a string to an integer
- * @n: the string to be converted
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
  */
 
-int _atoi(char *n)
+int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0; n[i] != '\0' && flag != 2; i++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
-		if (n[i] == '-')
+		if (s[i] == '-')
 			sign *= -1;
 
-		if (n[i] >= '0' && n[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (n[i] - '0');
+			result += (s[i] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
@@ -72,3 +72,4 @@ int _atoi(char *n)
 
 	return (output);
 }
+
