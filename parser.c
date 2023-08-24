3e76234ff967c3d9d5e9c7e07b73cbd9d03eeb88
Achrafans <122817393+Achrafans@users.noreply.github.com>
2023-08-24 16:01:02 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- * is_cmd1 - determines if a file is an executable command
+ * is_cmd - determines if a file is an executable command
  * @info: the info struct
  * @path: path to the file
  *
  * Return: 1 if true, 0 otherwise
  */
-int is_cmd1(info_t *info, char *path)
+int is_cmd(info_t *info, char *path)
 {
 	struct stat st;
 
@@ -23,14 +23,14 @@ int is_cmd1(info_t *info, char *path)
 }
 
 /**
- * dup_chars1 - duplicates characters
+ * dup_chars - duplicates characters
  * @pathstr: the PATH string
  * @start: starting index
  * @stop: stopping index
  *
  * Return: pointer to new buffer
  */
-char *dup_chars1(char *pathstr, int start, int stop)
+char *dup_chars(char *pathstr, int start, int stop)
 {
 	static char buf[1024];
 	int i = 0, k = 0;
@@ -43,14 +43,14 @@ char *dup_chars1(char *pathstr, int start, int stop)
 }
 
 /**
- * find_path1 - finds this cmd in the PATH string
+ * find_path - finds this cmd in the PATH string
  * @info: the info struct
  * @pathstr: the PATH string
  * @cmd: the cmd to find
  *
  * Return: full path of cmd if found or NULL
  */
-char *find_path1(info_t *info, char *pathstr, char *cmd)
+char *find_path(info_t *info, char *pathstr, char *cmd)
 {
 	int i = 0, curr_pos = 0;
 	char *path;
