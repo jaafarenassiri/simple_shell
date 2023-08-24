1f936689920b41565a49f33db9274ae62fd073ce
Achrafans <122817393+Achrafans@users.noreply.github.com>
2023-08-24 16:02:22 +0100

    Update string.c

--- string.c
+++ string.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * _strlen1 - returns the length of a string
+ * _strlen - returns the length of a string
  * @s: the string whose length to check
  *
  * Return: integer length of string
  */
-int _strlen1(char *s)
+int _strlen(char *s)
 {
 	int i = 0;
 
@@ -19,13 +19,13 @@ int _strlen1(char *s)
 }
 
 /**
- * _strcmp1 - performs lexicogarphic comparison of two strangs.
+ * _strcmp - performs lexicogarphic comparison of two strangs.
  * @s1: the first strang
  * @s2: the second strang
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
-int _strcmp1(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2)
 	{
@@ -41,13 +41,13 @@ int _strcmp1(char *s1, char *s2)
 }
 
 /**
- * starts_with1 - checks if needle starts with haystack
+ * starts_with - checks if needle starts with haystack
  * @haystack: string to search
  * @needle: the substring to find
  *
  * Return: address of next char of haystack or NULL
  */
-char *starts_with1(const char *haystack, const char *needle)
+char *starts_with(const char *haystack, const char *needle)
 {
 	while (*needle)
 		if (*needle++ != *haystack++)
@@ -56,13 +56,13 @@ char *starts_with1(const char *haystack, const char *needle)
 }
 
 /**
- * _strcat1 - concatenates two strings
+ * _strcat - concatenates two strings
  * @dest: the destination buffer
  * @src: the source buffer
  *
  * Return: pointer to destination buffer
  */
-char *_strcat1(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	char *ret = dest;
 
