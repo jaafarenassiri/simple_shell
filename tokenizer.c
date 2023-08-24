5d159a2de5c011b92204f62ff56f5081624e3238
Achrafans <122817393+Achrafans@users.noreply.github.com>
2023-08-24 16:02:53 +0100

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- * **strtow1 - splits a string into words. Repeat delimiters are ignored
+ * **strtow - splits a string into words. Repeat delimiters are ignored
  * @str: the input string
  * @d: the delimeter string
  * Return: a pointer to an array of strings, or NULL on failure
  */
 
-char **strtow1(char *str, char *d)
+char **strtow(char *str, char *d)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
@@ -49,12 +49,12 @@ char **strtow1(char *str, char *d)
 }
 
 /**
- * **strtow21 - splits a string into words
+ * **strtow2 - splits a string into words
  * @str: the input string
  * @d: the delimeter
  * Return: a pointer to an array of strings, or NULL on failure
  */
-char **strtow21(char *str, char d)
+char **strtow2(char *str, char d)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
